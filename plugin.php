c2104f6694bd67c418166ee3924bff35f3d8cbf6
Jameel Bokhari <jameelbokhari@gmail.com>
2014-12-28 16:57:55 -0700

    Added updater configuration for updating through github

--- plugin.php
+++ plugin.php
@@ -3,7 +3,7 @@
  * Plugin Name: Phone Number Swappy
  * Plugin URI: http://www.anchorwave.com
  * Description: Used to swap phone numbers
- * Version: 1.1.0
+ * Version: 1.0.0
  * Author: Jameel Bokhari
  * Author URI: http://www.codeatlarge.com
  * License: GPL2
